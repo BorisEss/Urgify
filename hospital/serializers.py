4d25a27703144c1c32be622b37eaa4d86d0e28a6
gheorghe <gheorghe.mirca@moovin.uk>
2022-08-18 17:37:55 +0300

    UR-94 created get/create departments endpoints

--- hospital/serializers.py
+++ hospital/serializers.py
@@ -8,3 +8,10 @@ class HospitalSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Hospital
         fields = ('name', 'slug', 'logo')
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Department
+        fields = ('name', 'slug', 'hospital_id')
--- src/settings.py
+++ src/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(
     DEBUG=(bool, False),
-    TOKEN_TTL=(int, 100)
+    TOKEN_TTL=(int, 5)
 )
 environ.Env.read_env()
 # Quick-start development settings - unsuitable for production
