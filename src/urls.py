d90e5dd9093bdc113d718784c6bd02df926990ff
gheorghe <gheorghe.mirca@moovin.uk>
2022-08-20 19:45:10 +0300

    changed swagger url

--- src/urls.py
+++ src/urls.py
@@ -31,9 +31,9 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('docs/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/', include([
+        path('docs/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        path('admin/', admin.site.urls),
         path('auth/', include('auth.urls')),
         path('hospitals/', include('hospital.urls')),
     ]))
