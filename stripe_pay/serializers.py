c1b59bc0f9a752767681412780a45d23ad50dbf4
gheorghe <gheorghe.mirca@moovin.uk>
2022-08-27 17:41:27 +0300

    allow any permission for stripe endpoint

--- stripe_pay/serializers.py
+++ stripe_pay/serializers.py
@@ -3,5 +3,8 @@ from rest_framework import serializers
 
 class StripePaymentIntentSerializer(serializers.Serializer):
     amount = serializers.IntegerField(min_value=0)
-    currency = serializers.CharField()
-    payment_method_types = serializers.ListField()
+    currency = serializers.ChoiceField(choices=('usd',))
+    payment_method_types = serializers.ChoiceField(choices=('card',))
+
+    def validate_payment_method_types(self, value):
+        return [value]
