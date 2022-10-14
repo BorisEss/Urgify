3e8ab0bcd1603603859f491ede640c558aa69531
gheorghe <gheorghe.mirca@moovin.uk>
2022-10-14 10:35:55 +0300

    UR-147 added permissions and changed models

---
+++ hospital/permissions.py
@@ -0,0 +1,14 @@
+# permissions.py
+
+from rest_framework import permissions
+
+from hospital.models import Hospital
+
+
+class MembershipPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if 'hospital_slug' in view.kwargs:
+            hospital = Hospital.objects.get(slug=view.kwargs['hospital_slug'])
+            return request.user in hospital.departments.all().values_list('employee')
+        return False
--- hospital/views.py
+++ hospital/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 
 from hospital import models
 from hospital import serializers
+from hospital.permissions import MembershipPermission
 
 
 class HospitalViewSet(viewsets.ModelViewSet):
@@ -13,9 +14,13 @@ class HospitalViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+    def get_queryset(self):
+        return models.Hospital.objects.filter(user=self.request.user)
+
 
 class DepartmentViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.DepartmentSerializer
+    permission_classes = (MembershipPermission,)
     lookup_field = 'slug'
 
     def get_queryset(self):
